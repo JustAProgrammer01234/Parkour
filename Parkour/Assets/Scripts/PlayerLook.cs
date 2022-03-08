b70b95ff3e2bc3bad1648b6ff1b7fb07ab8cb078
scripto_entity_1010 <scriptoentity@gmail.com>
2022-03-08 16:31:07 +0800

    Added PlayerLook.cs
    Added a script that allows the player to look around and made the player's color red.

---
+++ Parkour/Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    public float sensitivity;
+    public Transform playerCamera; 
+
+    float mouseX;
+    float mouseY;
+    float cameraRotation; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerCamera = this.gameObject.transform.GetChild(0);
+
+        mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        cameraRotation -= mouseY;
+        cameraRotation = Mathf.Clamp(cameraRotation, -90f, 90f);
+
+        transform.Rotate(Vector3.up * mouseX);
+        playerCamera.localRotation = Quaternion.Euler(cameraRotation, 0f, 0f); 
+    }
+}
