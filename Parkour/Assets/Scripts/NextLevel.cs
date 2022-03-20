02322da27f7550b7afa6fb26f02ec02b724e31da
scripto_entity_1010 <scriptoentity@gmail.com>
2022-03-20 12:06:22 +0800

    Added a lot of shit
    Added more scripts, scenes, a second level, and an fps thingy.

--- Parkour/Assets/Scripts/NextLevel.cs
+++ Parkour/Assets/Scripts/NextLevel.cs
@@ -1,11 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; 
 
 public class NextLevel : MonoBehaviour
 {
+    bool isCollided = false; 
+
+    void Update()
+    {
+        isCollided = false; 
+    }
+
     void OnTriggerEnter(Collider collision)
     {
-        Debug.Log(collision);
+        if (!isCollided)
+        {
+            StartCoroutine(GoToNextlevel());
+        }
+    }
+
+    IEnumerator GoToNextlevel()
+    {
+        AsyncOperation loadScene = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+
+        while (!loadScene.isDone)
+        {
+            yield return null; 
+        }
     }
 }
---
+++ Parkour/Assets/Scripts/SendFrameRate.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class SendFrameRate : MonoBehaviour
+{
+    public Text frameRate;
+    float deltaTime; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+        float fps = 1.0f / deltaTime;
+        frameRate.text = $"FPS: {Mathf.Ceil(fps)}";
+    }
+}
