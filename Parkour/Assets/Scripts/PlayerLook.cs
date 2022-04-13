8be53fea9743df98555e07e5b5c0b1eda7936495
scripto_entity_1010 <scriptoentity@gmail.com>
2022-04-13 13:06:48 +0800

    Fix go brrrr

--- Parkour/Assets/Scripts/PlayerLook.cs
+++ Parkour/Assets/Scripts/PlayerLook.cs
@@ -4,23 +4,20 @@ using UnityEngine;
 
 public class PlayerLook : MonoBehaviour
 {
-    public float sensitivity;
-    
     Transform playerCamera;
-    
-    float cameraRotation; 
+    float cameraRotation;
+    float sensitivity = 250f;
 
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        playerCamera = transform.GetChild(0); 
     }
 
     // Update is called once per frame
     void Update()
-    {
-        playerCamera = transform.GetChild(0);
-
+    { 
         float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
 
